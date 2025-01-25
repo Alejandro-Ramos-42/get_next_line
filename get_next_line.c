54f0a75206a2996c8099b3ad17097064851860c4
Alex <alejandro.ramos.gua@gmail.com>
2025-01-25 14:39:38 +0000

    testing basic read implementation

--- get_next_line.c
+++ get_next_line.c
@@ -14,15 +14,41 @@
 
 char	*get_next_line(int fd)
 {
-	char	*
-	read(fd, )
+	char	*buffer;
+
+  buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+  if (!buffer)
+    return (NULL);
+	read(fd, buffer, BUFFER_SIZE);
+  buffer[BUFFER_SIZE] = "\0"
+  printf("%s", buffer);
+  return(buffer);
+}
+
+find_line(char *str)
+{
+  char  *start;
+  char  *finish;
+  char  *line;
+  int   len;
+
+  start = str;
+  finish = str;
+  while (finish)
+  {
+    if (*finish == '\n')
+    {
+      line = malloc()
+    }
+  }
 }
 
 int	main(void)
 {
 	int	fd;
 
-	fd = open("test.txt", O_RDONLY | O_CREAT);
-	get_next_line(fd);
+	fd = open("test.txt", O_RDONLY);
+	printf("fd of test.txt = %d\n", fd);
+  get_next_line(fd);
 	return (0);
 }
