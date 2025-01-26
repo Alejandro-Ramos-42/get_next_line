cdbb95c9a0fa74c5a26adeec1bd898c06d81c75c
Alex <alejandro.ramos.gua@gmail.com>
2025-01-26 16:54:35 +0000

    added something

--- get_next_line.c
+++ get_next_line.c
@@ -12,43 +12,53 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	char	*buffer;
-
-  buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-  if (!buffer)
-    return (NULL);
-	read(fd, buffer, BUFFER_SIZE);
-  buffer[BUFFER_SIZE] = "\0"
-  printf("%s", buffer);
-  return(buffer);
-}
-
-find_line(char *str)
-{
-  char  *start;
-  char  *finish;
-  char  *line;
-  int   len;
-
-  start = str;
-  finish = str;
-  while (finish)
-  {
-    if (*finish == '\n')
-    {
-      line = malloc()
-    }
-  }
-}
-
+//char	*get_next_line(int fd)
+//{
+//	char	*buffer;
+//
+//  buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+//  if (!buffer)
+//    return (NULL);
+//	read(fd, buffer, BUFFER_SIZE);
+//  buffer[BUFFER_SIZE] = "\0"
+//  printf("%s", buffer);
+//  return(buffer);
+//}
+//
+//find_line(char *str)
+//{
+//  char  *start;
+//  char  *finish;
+//  char  *line;
+//  int   len;
+//
+//  start = str;
+//  finish = str;
+//  while (finish)
+//  {
+//    if (*finish == '\n')
+//    {
+//      line = malloc()
+//    }
+//  }
+//}
+//
 int	main(void)
 {
-	int	fd;
+	int	  fd;
+  char  *buffer;
+  int   chars_read;
+  int   buffer_size;
 
 	fd = open("test.txt", O_RDONLY);
-	printf("fd of test.txt = %d\n", fd);
-  get_next_line(fd);
+  buffer_size = 5;
+  buffer = (char *)malloc((buffer_size + 1) * sizeof(char));
+  while ((chars_read = read(fd, buffer, 10)))
+  {
+    buffer[chars_read] = '\0';
+    printf("buffer-> %s\n", buffer);
+  }
+  free(buffer);
+  //get_next_line(fd);
 	return (0);
 }
