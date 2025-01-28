14587119211b1a46437105beb7c667205a791589
Alex <alejandro.ramos.gua@gmail.com>
2025-01-28 19:47:55 +0000

    Up to today. Not much advanced. I implemented a bit the gind line function, but need to work on how to actually use the return value efficiently

--- get_next_line.c
+++ get_next_line.c
@@ -12,29 +12,34 @@
 
 #include "get_next_line.h"
 
-search_line(char *buffer)
+int search_line(char *buffer)
 {
-  s_list  *new_node;
+  int i;
 
-  while(buffer)
+  i = 0;
+  while(buffer[i])
   {
-    while (*buffer != '\n')
-    {
-      *new_node -> content = *buffer;
-    }
+    if (buffer[i] == '\n')
+      return (i);
+    i++;
   }
+  return (-1);
 }
 
 char	*get_next_line(int fd)
 {
-	char  *buffer;
-  int   chars_read;
+  static char  *leftover;
+	char         *buffer;
+  int          chars_read;
 
   buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));//6
   if (!buffer)
     return (NULL);
+  if (fd == 0 || read(fd, buffer, 0) == 0)
+    return (NULL);
 	chars_read = read(fd, buffer, BUFFER_SIZE);//5
   buffer[BUFFER_SIZE] = '\0';//buffer = "Hello\0"
+  if search_line(buffer);
   printf("%s", buffer);
   return(buffer);
 }
